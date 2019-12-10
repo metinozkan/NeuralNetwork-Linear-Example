fcffccee8320e8c4e6f4a4298b6864707f81a823
metinozkan <metin.ozkn@hotmail.com>
2019-12-10 16:29:42 +0300

    added Data.txt,created main func

--- src/linearnn/LinearNN.java
+++ src/linearnn/LinearNN.java
@@ -1,21 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package linearnn;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  *
- * @author casemice
+ * @author metinozkan
  */
 public class LinearNN {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+         NeuralN ysa=new NeuralN();
+         Scanner in =new Scanner (System.in);
+         int select=0;
+         
+         do{
+             System.out.println("1-Training network");
+             System.out.println("2-Testing network");
+             System.out.println("3-Exit");
+             
+             select=in.nextInt();
+             
+             switch(select){
+                 case 1:ysa.Training();
+                 System.out.println("Training");
+                 break;
+                 
+                 case 2:
+                     System.out.println("x:");
+                     double x=in.nextDouble();
+                     System.out.println("y:");
+                     double y=in.nextDouble();
+                     double []z=ysa.Test(x, y);
+                     System.out.println("z:"+z[0]);
+                     System.in.read();
+                     break;
+             }
+         }while(select!=3);
+    }
     }
     
-}
---
+++ src/linearnn/NeuralN.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linearnn;
+
+import java.io.File;
+
+/**
+ *
+ * @author metinozkan
+ */
+public class NeuralN {
+  
+    private static final File file = new File("Data.txt");
+    
+  
+    
+}
